bf761988238feeecd3f95a82a0cb052d1dfd0a8a
neslihanalcglu <50194817+neslihanalcglu@users.noreply.github.com>
2022-07-11 06:11:49 +0300

    adding endpoints MovieController

--- MovieRestful.API/Controllers/MovieController.cs
+++ MovieRestful.API/Controllers/MovieController.cs
@@ -7,7 +7,7 @@ using MovieRestful.Core.Services;
 
 namespace MovieRestful.API.Controllers
 {
-    
+
     public class MovieController : CustomBaseController
     {
         private readonly IMapper _mapper;
@@ -19,13 +19,48 @@ namespace MovieRestful.API.Controllers
             _service = service;
         }
 
-        public async Task<ActionResult> GetList()
+        [HttpGet]
+        public async Task<ActionResult> GetListAsync()
         {
             var movies = await _service.GetAllAsync();
-            
-            var movieDtos= _mapper.Map<List<MovieDto>>(movies.ToList());//Liste IEnumerable döndüğü için listeye çevrildi
-            return Ok( CustomResponseDto<List<MovieDto>>.Success(200, movieDtos));
+
+            var movieDtos = _mapper.Map<List<MovieDto>>(movies.ToList());//Liste IEnumerable döndüğü için listeye çevrildi
+            return Ok(CustomResponseDto<List<MovieDto>>.Success(200, movieDtos));
             //return CreateActionResult(CustomResponseDto<List<MovieDto>>.Success(200, movieDtos));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetAsync(int id)
+        {
+            var movie = await _service.GetByIdAsync(id);
+
+            var movieDto = _mapper.Map<List<MovieDto>>(movie);
+            return Ok(CustomResponseDto<List<MovieDto>>.Success(200, movieDto));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateAsync(MovieDto input)
+        {
+            var movie = await _service.AddAsync(_mapper.Map<Movie>(input));
+
+            var movieDto = _mapper.Map<MovieDto>(movie);
+            return Ok(CustomResponseDto<MovieDto>.Success(201, movieDto));// 201-Created
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateAsync(MovieDto input)
+        {
+            await _service.UpdateAsync(_mapper.Map<Movie>(input));
+            return Ok(CustomResponseDto<NoContentDto>.Success(204));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> DeleteAsync(int id)
+        {
+            var movie = await _service.GetByIdAsync(id);
+            await _service.RemoveAsync(movie);
+
+            return Ok(CustomResponseDto<NoContentDto>.Success(204));
         }
     }
 }
---
+++ MovieRestful.Core/Dtos/NoContentDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRestful.Core.Dtos
+{
+    public class NoContentDto
+    {
+        //null göndermek yerine daha düzgün bir görüntü için boş bir class oluşturuldu
+    }
+}
